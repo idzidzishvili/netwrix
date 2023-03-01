3d7ab227e15f4e8c3ce6a870fee1e213bc59ebf2
Ilia <ilia.dzidzishvili@gmail.com>
2023-03-02 00:16:05 +0400

    models, migrations, seeders

---
+++ database/migrations/2023_03_01_200736_create_loc_state_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loc_state', function (Blueprint $table) {
+            $table->unsignedBigInteger('state_id')->autoIncrement();
+            $table->string('name');
+            $table->string('short_name', 50)->nullable();
+            $table->bigInteger('country_id')->unsigned();
+
+            $table->index(['country_id', 'short_name', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loc_state');
+    }
+};
